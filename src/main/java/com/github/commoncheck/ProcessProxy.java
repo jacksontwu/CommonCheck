e1ded06cfa6e7c78b8a7ec5c0d40fa577a05e13a
jacksonwu1992 <59786894+jacksonwu1992@users.noreply.github.com>
2021-12-25 00:03:57 +0800

    feat: 1.0.1

--- src/main/java/com/github/commoncheck/ProcessProxy.java
+++ src/main/java/com/github/commoncheck/ProcessProxy.java
@@ -1,37 +1,28 @@
 package com.github.commoncheck;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
+public class ProcessProxy<T extends CommonProcess<P, Q>, P, Q> implements DynamicProxy<T, P, Q>, InvocationHandler {
+    T target;
 
-public class ProcessProxy<T extends CommonProcess<P, Q>, P, Q> implements DynamicProxy<T, P, Q>, MethodInterceptor {
-    private T t;
-
-    public ProcessProxy(T t) {
-        this.t = t;
+    public ProcessProxy(T target) {
+        this.target = target;
     }
 
     @Override
-    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
-        method.invoke(t, objects);
-        return null;
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object result = method.invoke(target, args);
+        return result;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public Q process(P p) {
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(t.getClass());
-        enhancer.setCallback(this);
-        T t = (T) enhancer.create();
-        Q q = t.check(p);
+        Q q = target.check(p);
         if (q != null) {
             return q;
         }
-        q = t.process(p);
+        q = target.process(p);
         return q;
     }
-
 }
